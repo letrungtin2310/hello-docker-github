8b0f0619b0685bd7d95d6cc3f500f74911d64250
zomzem-audepartment <zomzem.audepartment@gmail.com>
2025-08-07 09:21:07 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require('http')
 const port = process.env.PORT || 3000
 http
   .createServer((req, res) => {
-    res.end('Hello Docker & GitHub Actions!')
+    res.end(
+      'Hello Docker & GitHub Actions!\n Current time: ' +
+        new Date().toISOString()
+    )
   })
   .listen(port, () => {
     console.log(`App running on port ${port}`)
